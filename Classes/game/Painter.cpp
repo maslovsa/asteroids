918af308a54cf4187e3d2915cbf81c0686f8ce59
maslovsa <s.maslov@corpwebgames.com>
2014-12-02 11:16:50 +0300

    add video

--- Classes/game/Painter.cpp
+++ Classes/game/Painter.cpp
@@ -30,29 +30,7 @@ void Painter::drawShip(const Ship *ship) {
     glDisableVertexAttribArray(colorSlot);
 
     if (ship->jet_[JET_FRONT]) {
-        mat4 rotation = mat4::Rotate(ship->getAngle());
-        mat4 scale = mat4::Scale(ship->scale / 2);
-        mat4 translation = mat4::Translate(0, 0, -1);
-        GLint modelviewUniform = glGetUniformLocation(m_simpleProgram, "Modelview");
-        mat4 modelviewMatrix = scale * rotation * translation;
-        glUniformMatrix4fv(modelviewUniform, 1, 0, modelviewMatrix.Pointer());
-
-        GLuint positionSlot = glGetAttribLocation(m_simpleProgram, "Position");
-        GLuint colorSlot = glGetAttribLocation(m_simpleProgram, "SourceColor");
-
-        glEnableVertexAttribArray(positionSlot);
-        glEnableVertexAttribArray(colorSlot);
-
-        GLsizei stride = sizeof(Vertex);
-        const GLvoid *pCoords = &ship->body[0].Position.x;
-        const GLvoid *pColors = &ship->body[0].Color.x;
-        glVertexAttribPointer(positionSlot, 2, GL_FLOAT, GL_FALSE, stride, pCoords);
-        glVertexAttribPointer(colorSlot, 4, GL_FLOAT, GL_FALSE, stride, pColors);
-
-        glDrawArrays(GL_LINE_LOOP, 0, (int) ship->body.size());
-
-        glDisableVertexAttribArray(positionSlot);
-        glDisableVertexAttribArray(colorSlot);
+        // may be some JET-animation
     }
 }
 
